51051014e2ae49ffa5ef91ce6d050011558ede20
Brandon Tran <brandontran444@gmail.com>
2024-02-09 23:47:20 -0600

    Create fittedline function

---
+++ R/fittedline.R
@@ -0,0 +1,15 @@
+#' Fitted line model for quad.lm
+#'
+#' @param x - BHDiameter value
+#'
+#' @return predicted height value
+#' @export
+#'
+#' @examples
+#' SPRING244753tran0195::myplot(x)
+#'
+#' '(predicted value)'
+#'
+myplot=function(x){
+  0.86089580 +1.46959217*x  -0.02745726*x^2
+}
